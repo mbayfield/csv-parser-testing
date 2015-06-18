cd153d6f817181a130b1b232342208b2dcad5b42
mbayfield <mbayfield@gmail.com>
2015-06-18 22:20:44 +1000

    Updating to use es6 syntax

---
+++ README.md
@@ -0,0 +1,10 @@
+CSV Parsing test
+================
+
+based on code in this stackoverflow: http://stackoverflow.com/questions/16831250/how-to-convert-csv-to-json-in-node-js
+
+Testing es6
+
+## Usage
+
+node --harmony index.js
--- index.js
+++ index.js
@@ -8,27 +8,30 @@ var s = require("underscore.string");
 var filePath = path.join(__dirname, 'data/2015-2016-australianpublicholidays.csv');
 
 // Read CSV
-var f = fs.readFileSync(filePath, {encoding: 'utf-8'}, 
-    function(err){console.log(err);});
+var f = fs.readFileSync( filePath, { encoding: 'utf-8'}, function(err)
+    	{console.log(err);
+    });
 
 // Split on row
 f = f.split("\r\n");
 
 // Get first row for column headers
-headers = f.shift().split(",");
-console.log(headers);
+const headers = f.shift().split(",");
+// console.log(headers);
 
-var json = [];    
-f.forEach(function(d){
+const json = [];    
+f.forEach( (d) => {
     // Loop through each row
-    tmp = {}
-    row = d.split(",")
-    for(var i = 0; i < headers.length; i++){
+    var tmp = {};
+    var row = d.split(",");
+    
+    for(var i = 0; i < headers.length; i++) {
         tmp[headers[i]] = s(row[i]).trim().value();
     }
+    
     // Add object to list
     json.push(tmp);
 });
 
 // output json to console
-console.log(json);
+// console.log(json);
